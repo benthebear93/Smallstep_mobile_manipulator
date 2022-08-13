9b8c7d2e976a866fcd1be87e736265b7007777f6
benthebear93 <nswve@naver.com>
2022-08-13 22:34:51 +0900

    URDF from Fusion all parts

--- ssmm_robot_description/launch/display.launch.py
+++ ssmm_robot_description/launch/display.launch.py
@@ -9,9 +9,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('ssmm_robot_test_description')
+    share_dir = get_package_share_directory('ssmm_robot_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'ssmm_robot_test.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'ssmm_robot.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
 
--- ssmm_robot_description/launch/gazebo.launch.py
+++ ssmm_robot_description/launch/gazebo.launch.py
@@ -1,7 +1,7 @@
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 import os
@@ -10,11 +10,14 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('ssmm_robot_test_description')
+    share_dir = get_package_share_directory('ssmm_robot_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'ssmm_robot_test.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'ssmm_robot.xacro')
     robot_description_config = xacro.process_file(xacro_file)
+
+    rviz_file = "display.rviz"
     robot_urdf = robot_description_config.toxml()
+    rviz_config = os.path.join(share_dir, "rviz", rviz_file)
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -53,21 +56,40 @@ def generate_launch_description():
             ])
         ])
     )
-
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
     urdf_spawn_node = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
-            '-entity', 'ssmm_robot_test',
-            '-topic', 'robot_description'
+            '-entity', 'ssmm_robot',
+            '-topic', 'robot_description',
+            '-x', str(0),
+            '-y', str(0),
+            '-z',str(0.1),
+            '-R', str(1.5708),
+            '-P', str(0),
+            '-Y', str(0)
         ],
         output='screen'
     )
 
+    rviz_start = ExecuteProcess(
+        cmd=["ros2", "run", "rviz2", "rviz2", "-d", rviz_config], output="screen"
+    )
+
     return LaunchDescription([
-        robot_state_publisher_node,
-        joint_state_publisher_node,
         gazebo_server,
         gazebo_client,
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
         urdf_spawn_node,
+        TimerAction(
+            period=3.0,
+            actions=[rviz_start]
+        )
     ])
